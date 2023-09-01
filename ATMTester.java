e8672fad6bf2bfae1610872c5432579c52af15fc
Chris <chris.r.weng@gmail.com>
2023-09-01 10:27:40 -0700

    Minor tester edits
    Checked audit as well

--- ATMTester.java
+++ ATMTester.java
@@ -1,16 +1,16 @@
 public class ATMTester {
     public static void main(String[] args) throws Exception {
         ATM.openAccount("Chris", 100);
-        ATM.openAccount("Chris", 200);
+        /* ATM.openAccount("Chris", 200); */
         ATM.openAccount("Ronan", 10);
         ATM.openAccount("Aidan", 10000);
-        ATM.closeAccount("Ronan");
+        /* ATM.closeAccount("Ronan"); */
         ATM.withdrawMoney("Ronan", 10);
         ATM.closeAccount("Ronan");
-        ATM.depositMoney("Ronan", 100);
+        /* ATM.depositMoney("Ronan", 100); */
         ATM.depositMoney("Chris", 1000);
         ATM.transferMoney("Aidan", "Chris", 5000);
-        ATM.transferMoney("Chris", "Aidan", 100000);
+        /* ATM.transferMoney("Chris", "Aidan", 100000); */
         ATM.audit();
     }
 }
